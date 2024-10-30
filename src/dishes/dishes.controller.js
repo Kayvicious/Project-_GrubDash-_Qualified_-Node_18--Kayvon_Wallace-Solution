7f9b3ad958890726a99580f49cb19c6ff67b28e3
Kayvon <kayvonwallace@gmail.com>
2024-10-30 10:27:49 -0700

    I have updated and a route for to create dishes, a the controller for creating dishes as well.

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,21 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+function create (req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = { 
+    id: nextId,
+    name: name,
+    description: description ,
+    price: price,
+    image_url: image_url,
+  };
+  urls.push(newDish);
+  res.status(201).json({ data: newDish})
+}
+
+module.exports = {
+  create: [create],
+  
+}
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,10 @@
+
 const router = require("express").Router();
+const controller = require("./dishes.controller.js")
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+router.route("/").post(controller.create)
 
 module.exports = router;
+
+
